9d09fbd1d9308fb763d4f59c8cc29fc7e0009d21
OkolJr <151054946+OkolJr@users.noreply.github.com>
2024-09-14 10:59:36 +0300

    fix

--- Algorithms/Fibonacci/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Fibonacci;
-
-int output = FibonacciRecursiveImplementation.Fibonacci(7);
-
-Console.WriteLine(output);
-
-output = FibonacciIterativeImplementation.Fibonacci(7);
-
-Console.WriteLine(output);
